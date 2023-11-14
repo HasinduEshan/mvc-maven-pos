e02184051723274f9d15625997b6030f9970a8e7
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-11-14 12:03:07 +0530

    create customer form and controller

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -1,4 +1,56 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomerFormController {
+
+    @FXML
+    private TableColumn colAddress;
+
+    @FXML
+    private TableColumn colId;
+
+    @FXML
+    private TableColumn colName;
+
+    @FXML
+    private TableColumn colOption;
+
+    @FXML
+    private TableColumn colSalary;
+
+    @FXML
+    private TableView tblCustomer;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void reloadButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateButtonOnAction(ActionEvent event) {
+
+    }
+
 }
