dea3a1b03d1e6b750d3c82ec7cb2bfc50aa2b5b8
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-11-18 16:38:01 +0530

    load table and delete item

--- src/main/java/controller/ItemFormController.java
+++ src/main/java/controller/ItemFormController.java
@@ -1,25 +1,33 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import db.DBConnection;
 import dto.CustomerDto;
 import dto.ItemDto;
+import dto.tm.CustomerTm;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import dto.tm.ItemTm;
 
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ItemFormController {
 
@@ -60,7 +68,66 @@ public class ItemFormController {
     private JFXTextField txtUnitPrice;
 
     public void initialize(){
+        colCode.setCellValueFactory(new TreeItemPropertyValueFactory<>("code"));
+        colDesc.setCellValueFactory(new TreeItemPropertyValueFactory<>("desc"));
+        colUnitPrice.setCellValueFactory(new TreeItemPropertyValueFactory<>("unitPrice"));
+        colQty.setCellValueFactory(new TreeItemPropertyValueFactory<>("qty"));
+        colOption.setCellValueFactory(new TreeItemPropertyValueFactory<>("btn"));
+        loadItemTable();
+    }
 
+    private void loadItemTable() {
+        ObservableList<ItemTm> tmList = FXCollections.observableArrayList();
+        String sql = "SELECT * FROM item";
+
+        try {
+            Statement stm = DBConnection.getInstance().getConnection().createStatement();
+            ResultSet result = stm.executeQuery(sql);
+
+            while (result.next()){
+                JFXButton btn = new JFXButton("Delete");
+
+                ItemTm tm = new ItemTm(
+                        result.getString(1),
+                        result.getString(2),
+                        result.getDouble(3),
+                        result.getInt(4),
+                        btn
+                );
+
+                btn.setOnAction(actionEvent -> {
+                    deleteItem(tm.getCode());
+                });
+
+                tmList.add(tm);
+            }
+
+            TreeItem<ItemTm> treeItem = new RecursiveTreeItem<>(tmList, RecursiveTreeObject::getChildren);
+            tblItem.setRoot(treeItem);
+            tblItem.setShowRoot(false);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteItem(String code) {
+        String sql = "DELETE from item WHERE code=?";
+
+        try {
+            PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
+            pstm.setString(1,code);
+            int result = pstm.executeUpdate(sql);
+            if (result>0){
+                new Alert(Alert.AlertType.INFORMATION,"Item Deleted!").show();
+                loadItemTable();
+            }else{
+                new Alert(Alert.AlertType.ERROR,"Something went wrong!").show();
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -92,7 +159,7 @@ public class ItemFormController {
             int result = pstm.executeUpdate();
             if (result>0){
                 new Alert(Alert.AlertType.INFORMATION,"Item Saved!").show();
-//                loadCustomerTable();
+                loadItemTable();
 //                clearFields();
             }
 
