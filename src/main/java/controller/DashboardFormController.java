fc73dec0bf38020b3d77d6891960ed700df0254d
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-11-25 14:37:28 +0530

    place order form added

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -55,4 +55,16 @@ public class DashboardFormController {
             e.printStackTrace();
         }
     }
+
+    public void placeOrderButtonOnAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/PlaceOrderForm.fxml"))));
+            stage.setResizable(true);
+            stage.setTitle("Place Order Form");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/controller/PlaceOrderFormController.java
+++ src/main/java/controller/PlaceOrderFormController.java
@@ -1,4 +1,9 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class PlaceOrderFormController {
+    public void backButtonOnAction(ActionEvent actionEvent) {
+
+    }
 }
